3afa1d696ed7035c2dc121f085e73ad1c94f8c74
Charlie Griefer <charlie@griefer.com>
2023-03-11 15:36:15 -0700

    Do some renames

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from app import create_app, db
 from app.models import User
 
-application = create_app()
+rphoenixthings = create_app()
 
 
-@application.shell_context_processor
+@rphoenixthings.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User}
